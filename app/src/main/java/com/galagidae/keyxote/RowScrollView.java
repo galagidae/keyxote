594380e8a4c593f8c4c19744cb9ee740ae5a6c94
Anthony Benbrook <anthonybenbrook@hotmail.com>
2020-01-29 18:16:17 +0000

    Add locked ScrollView

---
+++ app/src/main/java/com/galagidae/keyxote/RowScrollView.java
@@ -0,0 +1,29 @@
+package com.galagidae.keyxote;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class RowScrollView extends ScrollView {
+    public RowScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_UP:
+                // if we can scroll pass the event to the superclass
+                return false;
+            default:
+                return super.onTouchEvent(ev);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return  false;
+    }
+}
